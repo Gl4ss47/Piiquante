a9ff09c994085d69bd48897ebff643765cfbb50d
Glass <Gl4ss@hotmail.fr>
2022-09-14 01:01:56 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+
+//connection a la base de donnée MongoDB 
+mongoose.connect('mongodb+srv://Flavien:YRyvYhCARivptudl@clusterpiiquante.dcdozum.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+  useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//Middleware des CORS (Cross Origin Resource Sharing)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/stuff', (req, res, next) => {
+    res.status(200).json(stuff);
+  });
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+      message: 'Objet créé !'
+    });
+  });
